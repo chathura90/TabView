8e7b5347431b403f5b472b0cd64e280886a1027e
hSenid <hsenid@vishwajithsmini>
2018-06-04 12:13:42 +0530

    Initial Commit

---
+++ TabExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabExample
+//
+//  Created by hSenid on 6/4/18.
+//  Copyright © 2018 hSenid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
