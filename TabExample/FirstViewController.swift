49570dfdc3a724cd72900aae85fbcbdbf829fea6
hSenid <hsenid@192.168.1.6>
2018-06-05 18:06:30 +0530

    alert and actionsheet implemented

--- TabExample/FirstViewController.swift
+++ TabExample/FirstViewController.swift
@@ -13,6 +13,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        userDefaults()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,40 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func userDefaults(){
+        let defaults = UserDefaults.standard
+        
+        // Storing Values of different times
+        defaults.set(15, forKey: "weight")
+        defaults.set(true, forKey: "isActive")
+        defaults.set(25.8, forKey: "distance")
+        defaults.set("Sergey", forKey: "firstName")
+        defaults.set(NSDate(), forKey: "currentDate")
+        
+        let friendsNames = ["Bill", "John"]
+        defaults.set(friendsNames, forKey: "friendsNames")
+        
+        // Reading from NSUserDefaults.
+        let weightValue = defaults.integer(forKey: "weight")
+        print("weight value = \(weightValue)")
+        
+        let isUserActive = defaults.bool(forKey: "isActive")
+        print("isUserActive value = \(isUserActive)")
+        
+        let distanceValue = defaults.double(forKey: "distance")
+        print("distance value = \(distanceValue)")
+        
+        let firstNameValue = defaults.object(forKey: "firstName") as! String
+        print("First Name value = \(firstNameValue)")
+        
+        let currentDateValue = defaults.object(forKey: "currentDate") as! NSDate
+        print("Current date value = \(currentDateValue)")
+    
+        
+        let friendsNamesArray = defaults.object(forKey: "friendsNames") as? [String] ?? [String]()
+        print("Friends names Array count \(friendsNamesArray.count)")
+        print("First element in array \(friendsNamesArray[0])")
+    }
 
 }
 
--- TabExample/SecondViewController.swift
+++ TabExample/SecondViewController.swift
@@ -20,6 +20,39 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onButtonClick(_ sender: Any) {
+        alertOnButtonClick()
+    }
+    
+    @IBAction func onActionSheetButtonClick(_ sender: Any) {
+        actionSheetOnButtonClick()
+    }
+    
+    func alertOnButtonClick(){
+        let alert = UIAlertController(title: "What's your name?", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Input your name here..."
+        })
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            
+            if let name = alert.textFields?.first?.text {
+                print("Your name: \(name)")
+            }
+        }))
+        
+        self.present(alert, animated: true)
+    }
 
+    func actionSheetOnButtonClick(){
+        let alert = UIAlertController(title: "Did you bring your towel?", message: "It's recommended you bring your towel before continuing.", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
 }
 
